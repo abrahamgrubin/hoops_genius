93aca31475f1048f6ea215b0933d800f1b1f0672
Charlie Gaines <beaugaines@gmail.com>
2014-09-15 10:57:11 -0400

    home and away team associations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ActiveRecord::Base
-  belongs_to :home_team, class_name: Team
-  belongs_to :away_team, class_name: Team
-  belongs_to :player
+  belongs_to :home_team, class_name: 'Team', foreign_key: :home_team_id
+  belongs_to :away_team, class_name: 'Team', foreign_key: :away_team_id
+  has_many :players
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,8 @@
 class Team < ActiveRecord::Base
 	belongs_to :user
   has_many :players
-	has_many :games
+  has_many :home_games, class_name: 'Game', foreign_key: :home_team_id
+	has_many :away_games, class_name: 'Game', foreign_key: :away_team_id
 	validates :name, presence: true, uniqueness: true  
   
 end
