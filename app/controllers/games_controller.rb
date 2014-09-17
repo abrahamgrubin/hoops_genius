e5b3afe8406a5d117bd0770437ce3d04abdfe3e0
Abraham Rubin <abrahamgrubin@gmail.com>
2014-09-17 11:09:42 -0300

    routing errors

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,16 +1,33 @@
 class GamesController < ApplicationController
   def new
+    @team = Team.find(params[:team_id])
     @game = Game.new
   end
   
   def create
-     @team = current_user.teams.find(params[:id])
-     @game = @team.home_games.create(params.require(:game).permit(:home_team_id, :away_team_id, :location, :date_of_game))
+     @team = Team.find(params[:team_id])
+     @game = @team.home_games.build(game_params)
+     #@alphabetical = Team.all.order(:name)
     if @game.save 
-    	redirect_to games_path
+    	redirect_to [@team, @game], notice: 'Game created'
+      #@team, @game is the routing issue. 
     else 
-    	flash[:error] = "There was an error creating your game"
-    	render :new
+      flash[:error] = "There was an error creating the game! Please try again" 
+      #flash error is not showing up. 
+      render :new
     end 
+  end
+
+  def show 
+    @team = Team.find(params[:team_id])
+    @game = @team.home_games.find(params[:id])
+    #this is where the issue is. It is also a routing issue
   end 
+
+  private 
+
+  def game_params 
+    params.require(:game).permit(:home_team_id, :away_team_id, :location, :date_of_game, :game_id)
+  end
+
 end
